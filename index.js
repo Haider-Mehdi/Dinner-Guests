aa180bbca389955ca3a8478016126d40ec39603c
Haider Mehdi <haidershah659@gmail.com>
2023-10-17 03:43:28 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Importing from exercise 16
+const More_Guests_1 = require("../More Guests");
+console.log(`We are inviting`, More_Guests_1.guestList.length, `people to the dinner.`);
---
+++ index.ts
@@ -0,0 +1,4 @@
+// Importing from exercise 16
+import{ guestList } from '../More Guests'
+ 
+console.log(`We are inviting`, guestList.length ,`people to the dinner.` )
